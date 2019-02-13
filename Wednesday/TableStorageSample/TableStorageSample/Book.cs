d498104d6fd53c0703f8d489dade58a655f77b1f
Christian Nagel <christian@christiannagel.com>
2019-02-13 16:40:22 +0100

    table storage sample

---
+++ Wednesday/TableStorageSample/TableStorageSample/Book.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TableStorageSample
+{
+    public class Book : TableEntity
+    {
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+    }
+}
---
+++ Wednesday/TableStorageSample/TableStorageSample/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TableStorageSample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            DefineConfiguration(args);
+            var account = CloudStorageAccount.Parse(Configuration["MyTableConnection"]);
+            var tableClient = account.CreateCloudTableClient();
+            var sampletable = tableClient.GetTableReference("mysampletable");
+            bool created = await sampletable.CreateIfNotExistsAsync();
+
+            var entity = new Book()
+            {
+                 PartitionKey = "BestBooksInTown",
+                 RowKey = Guid.NewGuid().ToString(),
+                 Title = "Professional C# 7",
+                 Publisher = "Wrox Press"
+            };
+            var operation = TableOperation.Insert(entity);
+            var result = await sampletable.ExecuteAsync(operation);
+           
+        }
+
+        public static IConfiguration Configuration { get; set; }
+
+        private static void DefineConfiguration(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddUserSecrets("MyTableStorageSample")
+                .AddCommandLine(args)
+                .Build();
+            Configuration = config;
+        }
+    }
+}
