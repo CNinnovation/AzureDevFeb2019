e2db59557b84f126a35a392df07168c2853f9b5a
Christian Nagel <christian@christiannagel.com>
2019-02-13 09:20:40 +0100

    asp.net core with ef core

---
+++ Wednesday/WebAppWithEFCore/WebAppWithEFCore/Models/BooksContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppWithEFCore.Models
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder.Entity<Book>().Property(b => b.Publisher).HasMaxLength(40)
+        //}
+    }
+}
